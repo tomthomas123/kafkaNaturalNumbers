edc63cfe615b627ffa73e82a0ffc1f5f1c57426c
tomthomas123 <tomthomas1248@gmail.com>
2022-11-08 12:06:02 +0530

    producer added

--- producer.py
+++ producer.py
@@ -1,3 +1,18 @@
 import time
 from kafka import KafkaProducer
 import random
+
+bootstrap_server = ["localhost:9092"]
+topic = "naturalnumbers"
+producer = KafkaProducer(bootstrap_servers = bootstrap_server)
+producer =KafkaProducer()
+
+def senddata():
+    data = random.randint(0,1000)
+    message = producer.send(topic,bytes(str(data),"utf-8"))
+    metadata =message.get()
+    print(metadata.topic)
+    print(metadata.partition)
+    time.sleep(3)
+while True:
+    senddata()
